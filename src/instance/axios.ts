9b2c02f21bfa36a219531a86111bf1e43b92ac0e
yazidzidanM <zidanotm@gmail.com>
2025-12-01 13:15:53 +0400

    try ngrok

--- src/instance/axios.ts
+++ src/instance/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "http://localhost:3112/api/v1",
+  baseURL: "https://tandy-multibladed-rhonda.ngrok-free.dev/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
