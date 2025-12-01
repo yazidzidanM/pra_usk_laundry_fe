f7c9e4690c1fbe7ef6888b9405f99249a61585cd
yazidzidanM <zidanotm@gmail.com>
2025-12-01 11:29:59 +0400

    fix

---
+++ src/app/dashboard/my_order/page.tsx
@@ -0,0 +1,5 @@
+export default function MyOrderPage(){
+  return (
+    <div>Order page</div>
+  )
+}
---
+++ src/app/dashboard/my_transaction/page.tsx
@@ -0,0 +1,5 @@
+export default function MyTrxPage(){
+  return (
+    <div>Trx page</div>
+  )
+}
