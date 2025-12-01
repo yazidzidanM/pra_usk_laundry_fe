950fba007fb53a4f14f7900b9f6a601d02bf5aed
yazidzidanM <zidanotm@gmail.com>
2025-12-01 15:57:57 +0400

    fix cookies v3

--- src/stores/auth-store.ts
+++ src/stores/auth-store.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 import jwt from "jsonwebtoken";
 import { redirect } from "next/navigation";
 
@@ -15,9 +15,16 @@ export async function getUserFromCookie(): Promise<Idecoded> {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
 
+  const h = await headers();
+  const isRSC =  h.get("rsc") === "1" || h.get("next-router-prefetch") === "1";
+
+  if (!token && isRSC) {
+    return {} as any; // biarkan prefetch tidak mengganggu
+  }
+
   if (!token) {
-    redirect("/login")
-  };
+    redirect("/login");
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET!) as Idecoded;
