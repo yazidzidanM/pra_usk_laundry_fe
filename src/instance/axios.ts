b78573a3dfc0943ca13befd299168b5cd24542b9
yazidzidanM <zidanotm@gmail.com>
2025-12-02 05:54:29 +0400

    debug v5

--- src/instance/axios.ts
+++ src/instance/axios.ts
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "https://tandy-multibladed-rhonda.ngrok-free.dev",
+  baseURL: "https://tandy-multibladed-rhonda.ngrok-free.dev/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true
 })
